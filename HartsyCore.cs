205eda1da9fbaa86c80bd281015155aa49cca54f
kalebbroo <kalebbroo@gmail.com>
2024-09-18 12:11:46 -0400

    initial setup

---
+++ HartsyCore.cs
@@ -0,0 +1,20 @@
+﻿using Hartsy.Extensions.Core.WebAPI;
+using SwarmUI.Core;
+using SwarmUI.Utils;
+
+namespace Hartsy.Extensions.Core
+{
+    public class HartsyCore : Extension
+    {
+        public override void OnFirstInit()
+        {
+            Logs.Debug("OpenWebUIExtension started.");
+            ScriptFiles.Add("Assets/hartsyCore.js");
+        }
+
+        public override void OnInit()
+        {
+            HartsyCoreAPI.Register();
+        }
+    }
+}
---
+++ WebAPI/HartsyCoreAPI.cs
@@ -0,0 +1,13 @@
+﻿using SwarmUI.WebAPI;
+
+namespace Hartsy.Extensions.Core.WebAPI
+{
+    [API.APIClass("API routes related to OpenWebUIExtension")]
+    public class HartsyCoreAPI
+    {
+        public static void Register()
+        {
+            //API.RegisterAPICall(PhoneHomeAsync, true);
+        }
+    }
+}
