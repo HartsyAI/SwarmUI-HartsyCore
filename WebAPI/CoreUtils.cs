c80a4055c91deb93a977b1d752b50c5c476af978
kalebbroo <kalebbroo@gmail.com>
2024-09-27 12:12:03 -0400

    Add Utils
    Utilities to be used with all Hartsy extensions. The hope is that this will reduce redundant code.
    
    Also some small fixes.

---
+++ WebAPI/CoreUtils.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using SwarmUI.WebAPI;
+
+namespace Hartsy.Extensions.Core.WebAPI
+{
+    public class CoreUtils : HartsyCoreAPI
+    {
+        public async static Task<JObject> CheckValidURL(
+            [API.APIParameter("URL to check")] string url)
+        {
+            JObject response = [];
+            try
+            {
+                Uri uri = new(url);
+                response["valid"] = true;
+                response["message"] = "The URL is valid.";
+            }
+            catch (UriFormatException ex)
+            {
+                response["valid"] = false;
+                response["message"] = "Invalid URL format.";
+                response["error"] = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                response["valid"] = false;
+                response["message"] = "An unexpected error occurred.";
+                response["error"] = ex.Message;
+            }
+            return await Task.FromResult(response);
+        }
+    }
+}
+
